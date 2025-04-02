4a9847d362c015cd1b946dad07e684848a24cc79
huytagama <luongthanhhuyptit@gmail.com>
2025-04-03 00:13:54 +0700

    front-end

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px]">
+        <div className="container">
+          <div className="flex items-center justify-between">
+
+            <div className="col1 w-[50%]">
+              <p className="text-[12px] font-[500]">
+                Get up to 50% off new season styles, limited time only
+              </p>
+            </div>
+
+            <div className="col2 flex items-center justify-end">
+              <ul>
+                <li className="list-none">
+                  <Link to="/" className="text-[12px] link">Help Center</Link>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
