87456a7438aba1900978a37eb4b79c0b4480dc8d
huytagama <luongthanhhuyptit@gmail.com>
2025-04-03 23:18:36 +0700

    update

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
+import Button from '@mui/material/Button';
 
 const Search = () => {
   return (
     <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px] relative p-2'>
         <input type='text' placeholder='Search for products...' className='w-full h-[35px] focus:outline-none bg-inherit p-2 text-[15px]' />
+        <Button variant="contained">Hello world</Button>
     </div>
   )
 }
