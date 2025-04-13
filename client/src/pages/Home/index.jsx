efae20b2b19890b6b4c66bd467b79c88f056c1b9
huytagama <luongthanhhuyptit@gmail.com>
2025-04-13 12:10:24 +0700

    create tab products

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,19 +3,65 @@ import HomeSlider from "../../components/HomeSlider";
 import HomeCatSlider from "../../components/HomeCatSlider";
 import { FaShippingFast } from "react-icons/fa";
 import AdsBannerSlider from "../../components/AdsBannerSlider";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const Home = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <>
       <HomeSlider />
       <HomeCatSlider />
+
+      <section className="py-8 bg-white">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <div className="leftSec">
+              <h2 className="text-[20px] font-[600]">Popular Products</h2>
+              <p className="text-[14px] font-[400]">
+                Do not miss the current offers until the end of March.
+              </p>
+            </div>
+
+            <div className="rightSec w-[50%]">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons="auto"
+                aria-label="scrollable auto tabs example"
+              >
+                <Tab label="Fashion" />
+                <Tab label="Electronics" />
+                <Tab label="Bags" />
+                <Tab label="Footwear" />
+                <Tab label="Beauty" />
+                <Tab label="Clothes" />
+                <Tab label="Groceries" />
+                <Tab label="Groceries" />
+                <Tab label="Groceries" />
+                <Tab label="Groceries" />
+                <Tab label="Groceries" />
+                <Tab label="Groceries" />
+              </Tabs>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <section className="py-16 bg-white">
         <div className="container">
           <div className="freeShipping w-full md:w-[80%] m-auto py-4 p-4 border-2 border-[#ff5252] flex items-center justify-center lg:justify-between flex-col lg:flex-row rounded-md mb-7">
             <div className="col1 flex items-center gap-4">
               <FaShippingFast className="text-[50px]" />
-              <span className="text-[16px] lg:text-[20px] font-[600] uppercase">Free Shipping</span>
+              <span className="text-[16px] lg:text-[20px] font-[600] uppercase">
+                Free Shipping
+              </span>
             </div>
 
             <div className="col2">
@@ -27,16 +73,22 @@ const Home = () => {
             <div className="col3">
               <p className="mb-0 font-bold text-[25px]">- Only $200*</p>
             </div>
-
           </div>
 
           <AdsBannerSlider items={4} />
         </div>
       </section>
 
-
-
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 };
